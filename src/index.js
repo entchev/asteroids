d17ecbd1c9b62f518de43fcfb41f4623e01fcdb2
entchev <micishi@gmail.com>
2021-01-16 18:54:06 +0000

    first commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+const MovingObject = require('../src/moving_object')
+console.log('Webpack is working!')
+
+window.MovingObject = MovingObject
---
+++ src/moving_object.js
@@ -0,0 +1,10 @@
+class MovingObject {
+  constructor(options) {
+    this.pos = options.pos
+    this.vel = options.vel
+    this.radius = options.radius
+    this.colour = options.colour
+  }
+}
+
+module.exports = MovingObject
