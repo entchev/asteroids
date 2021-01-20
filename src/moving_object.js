1ab6cce7889f21851a819d3cde50243bd7d1be66
entchev <micishi@gmail.com>
2021-01-20 21:19:03 +0000

    update

--- src/moving_object.js
+++ src/moving_object.js
@@ -13,6 +13,18 @@ class MovingObject {
     ctx.arc(this.pos[0], this.pos[1], this.radius, 0, 2 * Math.PI, true)
     ctx.fill()
   }
+
+  move(timeDelta) {
+    // timeDelta is number of milliseconds since last move
+    // if the computer is busy the time delta will be larger
+    // in this case the MovingObject should move farther in this frame
+    // velocity of object is how far it should move in 1/60th of a second
+    const velocityScale = timeDelta / NORMAL_FRAME_TIME_DELTA,
+      offsetX = this.vel[0] * velocityScale,
+      offsetY = this.vel[1] * velocityScale
+
+    this.pos = [this.pos[0] + offsetX, this.pos[1] + offsetY]
+  }
 }
 
 module.exports = MovingObject
